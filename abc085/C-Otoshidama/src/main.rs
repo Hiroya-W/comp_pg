e432beb98282e697091b2d0c3911eebdd1519789
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-11-25 22:21:00 +0900

    [add] 解くべき精選過去問10問を解いた 	new file:   abc081/B-Shift_Only/Cargo.toml

---
+++ abc085/C-Otoshidama/src/main.rs
@@ -0,0 +1,72 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!(
+        n : i32,
+        y : i32,
+    );
+    let mut ans = None;
+    'outer: for i in 0..n + 1 {
+        for j in 0..n - i + 1 {
+            let k = n - i - j;
+            if i * 10000 + j * 5000 + k * 1000 == y {
+                ans = Some((i, j, k));
+                break 'outer;
+            }
+        }
+    }
+    let (x, y, z) = ans.unwrap_or((-1, -1, -1));
+    println!("{} {} {}", x, y, z);
+}
