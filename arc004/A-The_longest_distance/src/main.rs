27fd6e68a62832f99381ed68c82075265092bd2c
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-12-08 23:40:11 +0900

    [add] abc147に参加

---
+++ arc004/A-The_longest_distance/src/main.rs
@@ -0,0 +1,76 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::cmp;
+
+fn main() {
+    input!(
+        N : i32,
+        point : [(i32,i32);N],
+    );
+    let mut ans : i32 = 0;
+    let point: Vec<(i32, i32)> = point;
+    for &v in &point{
+        for &u in &point {
+            let len = (v.0-u.0).pow(2)+(v.1-u.1).pow(2);
+            ans = if ans < len{
+              len
+            }
+            else{
+                ans
+            };
+        }
+    }
+    println!("{:.6}",(ans as f64).sqrt());
+}
