5be20b004f7a2f54637a57a9144faec55ff4fa11
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-12-14 21:24:54 +0900

    [update] abc147 C を解いた

--- abc147/C-HonestOrUnkind2/src/main.rs
+++ abc147/C-HonestOrUnkind2/src/main.rs
@@ -1,4 +1,3 @@
-
 // https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
 macro_rules! input {
     (source = $s:expr, $($r:tt)*) => {
@@ -48,11 +47,45 @@ macro_rules! read_value {
         read_value!($next, usize) - 1
     };
 
+    // https://atcoder.jp/contests/abc147/submissions/8921445
+    // 3
+    // 1 2
+    // 1 3
+    // 1 4
+    // のようなタイプ
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
     ($next:expr, $t:ty) => {
         $next().parse::<$t>().expect("Parse error")
     };
 }
 
 fn main() {
-    println!("Hello, world!");
+    input!(
+        N : i32,
+        arr : [[(i32,i32)];N]
+    );
+
+    let mut ans = 0;
+    for mask in 0..1 << N {
+        let mut ok = true;
+        for i in 0..N {
+            if mask >> i & 1 == 1 {
+                for j in 0..arr[i as usize].len() {
+                    let p = arr[i as usize][j as usize];
+                    // check contradiction
+                    if mask >> (p.0 - 1) & 1 ^ p.1 == 1 {
+                        ok = false;
+                    }
+                }
+            }
+        }
+        if ok && (ans as u32).count_ones() < (mask as u32).count_ones() {
+            ans = mask;
+        }
+    }
+    println!("{}", ans.count_ones());
 }
