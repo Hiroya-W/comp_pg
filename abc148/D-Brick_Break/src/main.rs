19628cab570aeb6513e7a65d36a5ba14cb3e0a7c
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2019-12-23 16:48:23 +0900

    [add] ABC148に参加した

---
+++ abc148/D-Brick_Break/src/main.rs
@@ -0,0 +1,87 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    // https://atcoder.jp/contests/abc147/submissions/8921445
+    // 3
+    // 1 2
+    // 1 3
+    // 1 4
+    // のようなタイプ
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!(
+        N : usize,
+        arr : [usize;N],
+    );
+
+    let N : usize = N;
+    let mut serch = 1;
+    let mut ans :i32 = 0;
+    for i in 0..N{
+        if arr[i] == serch{
+            serch += 1;
+        }
+        else {
+            ans += 1;
+        }
+    }
+    if ans == N as i32{
+        ans = -1;
+    }
+    println!("{}",ans);
+}
