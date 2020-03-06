3215c8b9b63749054fa614a021c3c93b7558c1d3
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2020-03-06 16:49:09 +0900

    [add] ABC157 D UnionFindを勉強した

---
+++ template/attool_cpp_temp.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+#define int long long
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+{% if mod %}
+const long long MOD = {{ mod }};
+{% endif %}
+{% if yes_str %}
+const string YES = "{{ yes_str }}";
+{% endif %}
+{% if no_str %}
+const string NO = "{{ no_str }}";
+{% endif %}
+
+{% if prediction_success %}
+void solve({{ formal_arguments }}){
+
+}
+{% endif %}
+
+int main(){
+    {% if prediction_success %}
+    {{input_part}}
+    solve({{ actual_arguments }});
+    {% else %}
+    // Failed to predict input format
+    {% endif %}
+    return 0;
+}
