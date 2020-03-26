c9817ba0c73db2e60566292093c71d9a5912672a
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2020-03-26 15:01:57 +0900

    [add] 過去問解いた

---
+++ abc017/A/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+typedef long long ll;
+
+
+void solve(std::vector<long long> s, std::vector<long long> e){
+
+}
+
+int main(){
+    std::vector<long long> s(3);
+    std::vector<long long> e(3);
+    for(int i = 0 ; i < 3 ; i++){
+        scanf("%lld",&s[i]);
+        scanf("%lld",&e[i]);
+    }
+    solve(std::move(s), std::move(e));
+    return 0;
+}
