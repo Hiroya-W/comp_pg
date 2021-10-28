3f31c003933e8160882a0f2d032061f603d72ffb
Hiroya-W <hyuyukun@gmail.com>
2021-10-28 23:07:25 +0900

    :+1: DP A

--- dp/a/main.cpp
+++ dp/a/main.cpp
@@ -32,6 +32,81 @@ inline bool chmin(T& a, T b) {
     return 0;
 }
 
+// もらうDP
+// int main() {
+//     int N;
+//     cin >> N;
+//     int h[100000 + 10];
+//     int dp[100000 + 10];
+
+//     rep(i, N) {
+//         cin >> h[i];
+//     }
+
+//     fill(dp, dp + 100000 + 10, INF);
+
+//     dp[0] = 0;
+
+//     rep1(i, N) {
+//         chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+//         if (i > 1) {
+//             chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+//         }
+//     }
+//     cout << dp[N - 1] << endl;
+
+//     return 0;
+// }
+
+// 配るDP
+// int main() {
+//     int N;
+//     cin >> N;
+//     int h[100000 + 10];
+//     int dp[100000 + 10];
+
+//     rep(i, N) {
+//         cin >> h[i];
+//     }
+
+//     fill(dp, dp + 100000 + 10, INF);
+
+//     dp[0] = 0;
+
+//     rep(i, N) {
+//         chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
+//         chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
+//     }
+//     cout << dp[N - 1] << endl;
+
+//     return 0;
+// }
+
+// メモ化再帰
+int h[100000 + 10];
+int dp[100000 + 10];
+
+int rec(int i) {
+    if (dp[i] < INF) return dp[i];
+
+    if (i == 0) return 0;
+    int res = INF;
+    chmin(res, rec(i - 1) + abs(h[i] - h[i - 1]));
+    if (i > 1) chmin(res, rec(i - 2) + abs(h[i] - h[i - 2]));
+    return dp[i] = res;
+}
+
 int main() {
+    int N;
+    cin >> N;
+
+    rep(i, N) {
+        cin >> h[i];
+    }
+
+    fill(dp, dp + 100000 + 10, INF);
+
+    cout << rec(N - 1) << endl;
+
     return 0;
 }
