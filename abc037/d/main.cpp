e9c988816a2d2c39b579b3849297be35596ddd2f
Hiroya-W <hyuyukun@gmail.com>
2021-11-20 22:57:48 +0900

    :+1: ABC 037 D

--- abc037/d/main.cpp
+++ abc037/d/main.cpp
@@ -32,6 +32,55 @@ inline bool chmin(T& a, T b) {
     return 0;
 }
 
+ll MOD = 1000000007;
+
+ll cnt_table[1000][1000];
+ll a[1000][1000];
+
+ll dfs(int i, int j) {
+    ll cnt = 1;
+    if (cnt_table[i][j] != 0) return cnt_table[i][j];
+
+    rep(k, 4) {
+        int ni = i + dx[k];
+        int nj = j + dy[k];
+
+        if (ni < 0 || ni >= 1000 || nj < 0 || nj >= 1000) continue;
+        if (a[ni][nj] > a[i][j]) {
+            cnt += dfs(ni, nj);
+            cnt %= MOD;
+        }
+    }
+    cnt_table[i][j] = cnt;
+    return cnt_table[i][j];
+}
+
 int main() {
+    int H, W;
+    cin >> H >> W;
+    rep(i, H) {
+        rep(j, W) {
+            int t;
+            cin >> t;
+            t--;
+            a[i][j] = t;
+        }
+    }
+
+    ll ans = 0;
+    rep(i, H) {
+        rep(j, W) {
+            ans = (ans + dfs(i, j)) % MOD;
+        }
+    }
+
+    // rep(i, H) {
+    //     rep(j, W) {
+    //         cout << cnt_table[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    cout << ans << endl;
+
     return 0;
 }
